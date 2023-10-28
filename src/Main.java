3f0fc801ad6c9213927dd04945c06563a4e0522c
Aswani K Mandava <aswani.k.mandava@gmail.com>
2023-10-28 18:41:44 -0400

    Replaced individual string opts with an opt array. Initial working version of Quiz game

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         QuestionService q = new QuestionService();
-        q.showQuestions();
-
+        q.startQuiz();
+        q.getScore();
+        System.out.println("------- GIVEN ANSWERS ------");
+        q.showGivenAnswers();
+        System.out.println("------- CORRECT ANSWERS ------");
+        q.showCorrectAnswers();
     }
 }
--- src/QuestionService.java
+++ src/QuestionService.java
@@ -1,19 +1,60 @@
+import java.util.Scanner;
+
 public class QuestionService {
-    private Question q[] = new Question[3];
+    private Question[] q = new Question[3];
+    private String[] answer = new String[3];
+    private int i = 0;
 
     public QuestionService() {
-        q[0] = new Question(0, "size of int in bytes", "2", "1", "8", "4", "4");
-        q[1] = new Question(0, "size of char in bytes", "1", "2", "4", "8", "2");
-        q[2] = new Question(0, "size of boolean in bytes", "4", "2", "1", "8", "1");
+        String[] q0Opts = { "2", "1", "8", "4" };
+        String[] q1Opts = { "1", "2", "4", "8" };
+        String[] q2Opts = { "4", "2", "1", "8" };
+        q[0] = new Question(1, "size of int in bytes", q0Opts, "4");
+        q[1] = new Question(2, "size of char in bytes", q1Opts, "2");
+        q[2] = new Question(3, "size of boolean in bytes", q2Opts, "1");
     }
 
-
-
-    public void showQuestions() {
-        // System.out.println(q[0].getQuestion());
+    public void startQuiz() {
         for (Question ques:q) {
-            // System.out.println(ques.getQuestion());
-            System.out.println(ques);
+            System.out.println("Q NO: " + ques.getId());
+            System.out.println(ques.getQuestion());
+            for(int j=0; j<ques.getOpt().length;j++) {
+                System.out.println(ques.getOpt()[j]);
+            }
+            // take an input from the user on the Question
+            Scanner sc = new Scanner(System.in);
+            System.out.print("INPUT: ");
+            answer[i] = sc.nextLine();
+            i++;
+        }
+    }
+
+    public void showGivenAnswers() {
+        int count = 1;
+        for (String ans:answer) {
+            System.out.println("Given answer for " + count + ": " + ans);
+            count++;
+
+        }
+    }
+
+    public void getScore() {
+        int score=0;
+        for (i=0; i < q.length; i++) {
+            String correct_answer = answer[i];
+            String submitted_answer = q[i].getAnswer();
+            if (correct_answer.equals(submitted_answer)) {
+                score++;
+            }
+        }
+        System.out.println("Your score: " + score);
+    }
+
+    public void showCorrectAnswers() {
+        int count=1;
+        for (i=0; i < q.length; i++) {
+            System.out.println("Answer for " + (count) + " - " + q[i].getQuestion() + ": " + q[i].getAnswer());
+            count++;
         }
     }
 }
